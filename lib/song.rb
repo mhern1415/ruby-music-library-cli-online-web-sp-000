774fd1b28b532fb27af9a7cd3205d1865fc0da34
Mark Hernandez <m.hern1415@gmail.com>
2019-06-23 23:25:27 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -56,6 +56,19 @@ end
 def self.find_or_create_by_name(name)
   self.find_by_name(name) || self.create(name)
 end
-  
-  
+
+def self.new_from_filename(name)
+  song_name = name.split(" - ")[1]
+  artist_name = name.split(" - ")[0]
+  genre_name = name.split(" - ")[2].chomp(".mp3")
+  song = self.find_or_create_by_name(song_name)
+  song.artist = Artist.find_or_create_by_name(artist_name)
+  song.genre = Genre.find_or_create_by_name(genre_name)
+  song
+end
+
+def self.create_from_filename(name)
+  @@all << self.new_from_filename(name)
+end
+
 end
